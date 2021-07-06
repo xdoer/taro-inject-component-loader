44e19ff808a10fe18201023af09e1db61f944354
Thomas Trainset <gotoanything@foxmail.com>
2021-07-06 17:16:37 +0800

    fix: 判断页面正则

--- src/index.ts
+++ src/index.ts
@@ -289,5 +289,5 @@ function insertComponent(node: any, componentName: string, state: any) {
 function defaultJudgePage(filePath: string) {
   // 兼容 windows 路径
   const formatFilePath = filePath.replace(/\\/g, '/')
-  return /(package-.+\/)?pages\/.+\/index\.[tj]sx$/.test(formatFilePath)
+  return /(package-.+\/)?pages\/[A-Za-z0-9-]+\/index\.[tj]sx$/.test(formatFilePath)
 }
